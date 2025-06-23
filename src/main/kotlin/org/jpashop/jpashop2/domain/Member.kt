666e21048b7eb8378a20832f42070ed3eecedf0f
socar-dep <dep@socar.kr>
2025-06-23 09:42:27 +0900

    kotlin에 Entity를 require로 설정하기 위해서는 플러그인 설정이 필요하다
    id 'org.jetbrains.kotlin.plugin.jpa' version '1.9.25'를 플러그인에 설정
    noArg {
        annotation(jakarta.persistence.Entity)
    }
    
    설정 추가

---
+++ src/main/kotlin/org/jpashop/jpashop2/domain/Member.kt
@@ -0,0 +1,16 @@
+package org.jpashop.jpashop2.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class Member(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
+    var id: Long? = null,
+    var name: String,
+)
