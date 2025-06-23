7495eda631fa0e1d31cb77a5d0de87fb5f13009c
socar-dep <dep@socar.kr>
2025-06-23 09:07:10 +0900

    first commit

---
+++ src/main/kotlin/org/jpashop/jpashop2/JpaShop2Application.kt
@@ -0,0 +1,11 @@
+package org.jpashop.jpashop2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaShop2Application
+
+fun main(args: Array<String>) {
+    runApplication<JpaShop2Application>(*args)
+}
---
+++ src/test/kotlin/org/jpashop/jpashop2/JpaShop2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.jpashop.jpashop2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaShop2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
