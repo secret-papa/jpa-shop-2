32f974b1e75e92aefcefbd4b711e32a1304933f5
socar-dep <dep@socar.kr>
2025-07-15 09:32:46 +0900

    Simple Order V2

--- src/main/kotlin/org/jpashop/jpashop2/api/OrderSimpleApiController.kt
+++ src/main/kotlin/org/jpashop/jpashop2/api/OrderSimpleApiController.kt
@@ -1,9 +1,12 @@
 package org.jpashop.jpashop2.api
 
+import org.jpashop.jpashop2.domain.Address
 import org.jpashop.jpashop2.domain.Order
+import org.jpashop.jpashop2.domain.OrderStatus
 import org.jpashop.jpashop2.repository.OrderRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
 class OrderSimpleApiController(
@@ -16,9 +19,29 @@ class OrderSimpleApiController(
 
         for(order in all) {
             order.member.name  // Lazy 강제 초기화
-            order.delivery.address  // Lazy 강제 초기화
+            order.delivery?.address  // Lazy 강제 초기화
         }
 
         return all
     }
+
+    @GetMapping("/api/v2/simple-orders")
+    fun ordersV2(): List<SimpleOrderDto> {
+        val all = orderRepository.findAll(1)
+        return all.map { o -> SimpleOrderDto(
+            orderId = o.id,
+            name = o.member.name,
+            orderDate = o.orderDate,
+            orderStatus = o.status,
+            address = o.delivery?.address
+        ) }
+    }
+
+    data class SimpleOrderDto(
+      val orderId: Long?,
+        val name: String,
+        val orderDate: LocalDateTime,
+        val orderStatus: OrderStatus,
+        val address: Address?
+    )
 }
